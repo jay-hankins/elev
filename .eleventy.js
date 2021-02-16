92184c3655c81729da47100cc9da07188850a25f
Jay Hankins <me@jhankins.dev>
2021-02-16 03:53:46 +0000

    Cool updates

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,7 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
-const markdownItFootnote = require("@gerhobbelt/markdown-it-footnote");
+const markdownItFootnote = require("markdown-it-footnote");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
