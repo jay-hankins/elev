b155672e4e5a5edb236ed0ac1b836ca1e70ee3c8
Jay Hankins <hankins.jay@gmail.com>
2024-01-31 10:09:18 -0800

    trying feed things

--- .eleventy.js
+++ .eleventy.js
@@ -26,6 +26,12 @@ module.exports = function (eleventyConfig) {
     );
   });
 
+  eleventyConfig.addFilter("readableDateTime", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+      "dd LLL yyyy hh:mm:ss"
+    );
+  });
+
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
   eleventyConfig.addFilter("htmlDateString", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
